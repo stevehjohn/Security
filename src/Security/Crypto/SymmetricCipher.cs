bae543240a39991a14f610770005bb2f90134774
Stevo John <stevehuwjohn@gmail.com>
2025-11-30 13:32:44 +0000

    Optional salt

--- src/Security/Crypto/SymmetricCipher.cs
+++ src/Security/Crypto/SymmetricCipher.cs
@@ -6,29 +6,39 @@ namespace Security.Crypto;
 
 public class SymmetricCipher : ISymmetricCipher
 {
-    public byte[] Encrypt(byte[] secret, byte[] key, byte[] iv, byte[] salt)
+    public byte[] Encrypt(byte[] secret, byte[] key, byte[] iv, byte[] salt = null)
     {
-        var cipherParameters = new AeadParameters(new KeyParameter(key), 128, iv, salt);
+        var cipherParameters = salt == null
+            ? new AeadParameters(new KeyParameter(key), 128, iv)
+            : new AeadParameters(new KeyParameter(key), 128, iv, salt);
+        
         var cipher = new GcmBlockCipher(new AesEngine());
+        
         cipher.Init(true, cipherParameters);
 
         var cipherText = new byte[cipher.GetOutputSize(secret.Length)];
 
         var len = cipher.ProcessBytes(secret, 0, secret.Length, cipherText, 0);
+        
         cipher.DoFinal(cipherText, len);
 
         return cipherText;
     }
 
-    public byte[] Decrypt(byte[] cipherText, byte[] key, byte[] iv, byte[] salt)
+    public byte[] Decrypt(byte[] cipherText, byte[] key, byte[] iv, byte[] salt = null)
     {
-        var cipherParameters = new AeadParameters(new KeyParameter(key), 128, iv, salt);
+        var cipherParameters = salt == null
+            ? new AeadParameters(new KeyParameter(key), 128, iv)
+            : new AeadParameters(new KeyParameter(key), 128, iv, salt);
+        
         var cipher = new GcmBlockCipher(new AesEngine());
+        
         cipher.Init(false, cipherParameters);
 
         var plainText = new byte[cipher.GetOutputSize(cipherText.Length)];
 
         var len = cipher.ProcessBytes(cipherText, 0, cipherText.Length, plainText, 0);
+        
         cipher.DoFinal(plainText, len);
 
         return plainText;
